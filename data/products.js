1d611f29a2577a9d01c190c2e7b89f5ac13f91e7
Harsh-Veliyat <harshveliyat@gmail.com>
2021-05-10 15:10:30 +0530

    First coomit

---
+++ data/products.js
@@ -0,0 +1,58 @@
+const products = [
+  {
+    name: "Harry Potter Box Set: The Complete Collection",
+    imageUrl:
+      "https://images.unsplash.com/photo-1606311698062-21c4f57cb27f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=695&q=80",
+    description:
+      "A beautiful boxed set containing all seven Harry Potter novels in paperback. These new editions of the classic and internationally bestselling, multi-award-winning series feature instantly pick-up-able new jackets by Jonny Duddle, with huge child appeal, to bring Harry Potter to the next generation of readers. It's time to PASS THE MAGIC ON.",
+    price: 1499,
+    countInStock: 15,
+  },
+  {
+    name: "Sculpture - from Antiquity to the Present Day ",
+    imageUrl:
+      "https://images.unsplash.com/photo-1619164025200-af10a99faeb3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "Ranging from ancient to contemporary sculpture, this book is the first study of the history of sculpture to present such an original and comprehensive approach. Taking the sculptures out of the museum context (and thus off of their proverbial pedestals), Sculpture presents a completely new view which affords enlightening comparisons between eras and genres. This remarkable work is indispensable for artlovers of all tastes and disciplines.",
+    price: 10,
+    countInStock: 32,
+  },
+  {
+    name: "The Bone Dragon ",
+    imageUrl:
+      "https://images.unsplash.com/photo-1616852247029-26ced7a70818?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "Evie's shattered ribs have been a secret for the last four years. Now she has found the strength to tell her adoptive parents, and the physical traces of her past are fixed - the only remaining signs a scar on her side and a fragment of bone taken home from the hospital, which her uncle Ben helps her to carve into a dragon as a sign of her strength.",
+    price: 8,
+    countInStock: 5,
+  },
+  {
+    name: "Wings of Fire: An Autobiography",
+    imageUrl:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1588286863l/634583._SY475_.jpg",
+    description:
+      "An Autobiography of A P J Abdul Kalam, former President of India. It was written by Dr. Abdul Kalam and Arun Tiwari. Dr. Kalam examines his early life, effort, hardship, fortitude, luck and chance that eventually led him to lead Indian space research, nuclear and missile programs.",
+    price: 6,
+    countInStock: 8,
+  },
+  {
+    name: "HAMLET: Play by William Shakespeare",
+    imageUrl:
+      "https://m.media-amazon.com/images/I/41ZprmZ5KqL.jpg",
+    description:
+      "The Tragedy of Hamlet, Prince of Denmark, often shortened to Hamlet, is a tragedy written by William Shakespeare sometime between 1599 and 1601. It is Shakespeare's longest play, with 29,551 words.",
+    price: 18,
+    countInStock: 32,
+  },
+  {
+    name: "The Alchemist",
+    imageUrl:
+      "https://images.unsplash.com/photo-1587547344587-3730713dd306?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "A special 25th anniversary edition of the extraordinary international bestseller, including a new Foreword by Paulo Coelho. Combining magic, mysticism, wisdom and wonder into an inspiring tale of self-discovery, The Alchemist has become a modern classic, selling millions of copies around the world and transforming the lives of countless readers across generations.",
+    price: 22,
+    countInStock: 13,
+  },
+];
+
+module.exports = products;
